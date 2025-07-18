311913f070990502549d9e8eaf181f91f051b288
keitagame <keitagame0319@icloud.com>
2025-07-18 14:49:16 +0900

    初回コミット

--- client/src/main.cpp
+++ client/src/main.cpp
@@ -183,7 +183,7 @@ int main(int argc, char* argv[]) {
         std::cout << "パッケージをインストールしています" << dest << "\n";
     }
     catch (const std::exception& e) {
-        std::cerr << "Error: " << e.what() << "\n";
+        std::cerr << "エラー" << e.what() << "\n";
         return 1;
     }
     return 0;
